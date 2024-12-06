59add5d023673391f957316a724068d661a20650
Sangavi G <Sangavikavi803@gmail.com>
2024-12-06 04:42:14 +0000

    commit1

---
+++ Practical1.java
@@ -0,0 +1,34 @@
+class Person
+{
+    String firstName;
+    String lastName;
+    int age;
+//Default constructor
+    public Person()
+ {
+    firstName = "";
+    lastName = "";
+    age = 0;
+}
+    public Person(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    }
+//Method to get full name
+    public String getFullName() {
+    return firstName +" "+lastName;
+    }
+    }
+    public class Practical1
+    {
+    public static void main (String[] args)
+    { 
+//Create two Person objects
+    Person person1 = new Person("Jhon", "doe", 30);
+    Person person2 = new Person("Alice", "Smith", 25);
+    System.out.println("Person 1: " + person1.getFullName());
+    System.out.println("Person 2: " + person2.getFullName());
+    double averageAge = (person1.age + person2.age)/2.0;
+    System.out.println("Average Age: " + averageAge);    }
+}
