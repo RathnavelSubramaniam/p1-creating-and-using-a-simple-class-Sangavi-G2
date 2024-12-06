c668e8589d4055a7a28ca8b8e19bb5ca49b9734f
Sangavi G <Sangavikavi803@gmail.com>
2024-12-06 05:05:40 +0000

    Commit1

--- Practical1.java
+++ Practical1.java
@@ -4,15 +4,15 @@ class Person
     String lastName;
     int age;
     public Person()
- {
-    firstName = "";
-    lastName = "";
-    age = 0;
-}
+    {
+          firstName = "";
+          lastName = "";
+          age = 0;
+    }
     public Person(String firstName, String lastName, int age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
     public String getFullName() {
     return firstName +" "+ lastName;
@@ -22,11 +22,11 @@ class Person
     {
     public static void main (String[] args)
     { 
-    Person person1 = new Person(" Jhon", " Doe", 30);
-    Person person2 = new Person(" Alice", " Smith", 25);
+    Person person1 = new Person("Jhon","Doe", 30);
+    Person person2 = new Person("Alice","Smith", 25);
     System.out.println("Person1: " + person1.getFullName());
     System.out.println("Person2: " + person2.getFullName());
-    double averageAge = (person1.age + person2.age) / 2.0;
+    double averageAge = (person1.age + person2.age)/2.0;
     System.out.println("Avarage age: " + averageAge);    
 }
 }
